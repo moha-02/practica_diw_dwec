21e170e26c72461b3a815b4587ee2feeb6f85d20
fabinho190 <ossai10fabianfbn@gmail.com>
2024-02-03 23:34:02 +0100

    Instalación webpack y boots

---
+++ surfweb/src/js/main.js
@@ -0,0 +1,4 @@
+import "../bootstrap-5.3.2/scss/bootstrap.scss";
+import "../bootstrap-5.3.2/scss/bootstrap-utilities.scss";
+import "../bootstrap-5.3.2/scss/bootstrap-reboot.scss";
+import "../bootstrap-5.3.2/scss/bootstrap-grid.scss";
