9a65a4e93c79c419ed129aeff01b175c76e35a83
fabinho190 <ossai10fabianfbn@gmail.com>
2024-02-07 21:15:18 +0100

    Login y register implentado

--- surfweb/src/js/crud.js
+++ surfweb/src/js/crud.js
@@ -1,3 +1,19 @@
+const divLogReg = document.querySelector("#navloginRegister");
+const divLogued = document.querySelector("#navlogued");
+const usuarioInicio = document.querySelector("#usuarioInicio");
+
+//Boton de loguarse
+
+const login = document.querySelector("#buttonSignin");
+login.addEventListener("click", checkLoguin);
+//Boton de registarse
+const registerCreate = document.querySelector("#buttonSignup");
+registerCreate.addEventListener("click", insertarUser);
+
+// Si hay un usuario , que se ponga
+const user = localStorage.getItem("User");
+usuarioInicio.innerHTML = user;
+
 const db = indexedDB.open("Surfweb", 1);
 
 db.onupgradeneeded = function (ev) {
@@ -6,13 +22,27 @@ db.onupgradeneeded = function (ev) {
   const usuarioObjectStore = database.createObjectStore("User", {
     keyPath: "email",
   });
-
-  usuarioObjectStore.createIndex("username", "username");
-  usuarioObjectStore.createIndex("password", "password");
 };
 
-const registerCreate = document.querySelector("#buttonSignup");
-registerCreate.addEventListener("click", insertarUser);
+function checkLoguin() {
+  const loginEmail = document.querySelector("#loginEmail").value;
+  const loginPassword = document.querySelector("#loginPassword").value;
+
+  const transaction = db.result.transaction(["User"], "readwrite");
+  const userTable = transaction.objectStore("User").getAll();
+  userTable.onsuccess = function (ev) {
+    const user = ev.target.result;
+    console.log("Hola");
+    for (let i = 0; i < user.length; i++) {
+      console.log("Hola");
+      if (user[i].email === loginEmail && user[i].password === loginPassword) {
+        localStorage.setItem("User", user[i].username);
+        divLogReg.setAttribute("hidden", "true");
+        divLogued.removeAttribute("hidden");
+      }
+    }
+  };
+}
 
 function insertarUser() {
   const username = document.querySelector("#registerUsername").value;
@@ -36,6 +66,9 @@ function insertarUser() {
       username: username,
       password: password,
     });
+    localStorage.setItem("User", username);
+    divLogReg.setAttribute("hidden", "true");
+    divLogued.removeAttribute("hidden");
   }
 }
 
