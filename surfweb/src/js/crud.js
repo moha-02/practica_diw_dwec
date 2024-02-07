9f5e49dc039c8caf4450a282926f63d32c28d7b2
fabinho190 <ossai10fabianfbn@gmail.com>
2024-02-07 02:18:58 +0100

    Validacion y base de datos

--- surfweb/src/js/crud.js
+++ surfweb/src/js/crud.js
@@ -11,15 +11,37 @@ db.onupgradeneeded = function (ev) {
   usuarioObjectStore.createIndex("password", "password");
 };
 
-function create() {
+const registerCreate = document.querySelector("#buttonSignup");
+registerCreate.addEventListener("click", insertarUser);
+
+function insertarUser() {
   const username = document.querySelector("#registerUsername").value;
   const email = document.querySelector("#registerEmail").value;
   const password = document.querySelector("#registerPassword").value;
+  const passwordConfirm = document.querySelector(
+    "#registerPasswordConfirm"
+  ).value;
 
-  const transaction = db.result.transaction(["User"], "readwrite");
-  transaction.objectStore("User").add({
-    email: email,
-    username: username,
-    password: password,
-  });
+  //Validación
+
+  if (
+    username != "" &&
+    isValidEmail(email) &&
+    password === passwordConfirm &&
+    password != ""
+  ) {
+    const transaction = db.result.transaction(["User"], "readwrite");
+    transaction.objectStore("User").add({
+      email: email,
+      username: username,
+      password: password,
+    });
+  }
+}
+
+//Pattern para el email
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^@\s]+$/;
+  return emailRegex.test(email);
 }
--- surfweb/src/js/main.js
+++ surfweb/src/js/main.js
@@ -4,6 +4,30 @@ import "../bootstrap-5.3.2/scss/bootstrap-reboot.scss";
 import "../bootstrap-5.3.2/scss/bootstrap-grid.scss";
 import "./crud";
 
+//Validacion formulario registro
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
 //Confirmación del password
 var registerPassword = document.getElementById("registerPassword");
 var registerPasswordConfirm = document.getElementById(
