b3502781c047a04c8eaa982c73a95fedb2af8245
fabinho190 <ossai10fabianfbn@gmail.com>
2024-02-08 14:07:15 +0100

    Filtrador tablas hecho

--- surfweb/src/js/main.js
+++ surfweb/src/js/main.js
@@ -3,6 +3,7 @@ import "../bootstrap-5.3.2/scss/bootstrap-utilities.scss";
 import "../bootstrap-5.3.2/scss/bootstrap-reboot.scss";
 import "../bootstrap-5.3.2/scss/bootstrap-grid.scss";
 import "./crud";
+import "./productos";
 
 //Validacion formulario registro
 (function () {
