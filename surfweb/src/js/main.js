bc2007779347a6aa548f35aad2552b1f876525f0
fabinho190 <ossai10fabianfbn@gmail.com>
2024-02-08 16:09:04 +0100

    merge fixe

--- surfweb/src/js/main.js
+++ surfweb/src/js/main.js
@@ -2,9 +2,6 @@ import "../bootstrap-5.3.2/scss/bootstrap.scss";
 import "../bootstrap-5.3.2/scss/bootstrap-utilities.scss";
 import "../bootstrap-5.3.2/scss/bootstrap-reboot.scss";
 import "../bootstrap-5.3.2/scss/bootstrap-grid.scss";
-<<<<<<< HEAD
-import "./weather";
-=======
 import "./crud";
 import "./productos";
 
@@ -48,4 +45,3 @@ function validatePassword() {
 
 registerPassword.onchange = validatePassword;
 registerPasswordConfirm.onkeyup = validatePassword;
->>>>>>> fabian
